3bab1680b7e11bc24a540291df353a8427915b1e
SandeshDhakal0 <dhakalsandesh0@gmail.com>
2022-07-25 16:12:30 +0545

    Initial

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CategoryController;
+
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/',[HomeController::class,'home']);
+Route::get('/admin/login',[AdminController::class,'login']);
+Route::post('/admin/login',[AdminController::class,'submit_login']);
+Route::get('/admin/dashboard',[AdminController::class,'dashboard']);
+Route::resource('/admin/category', CategoryController::class);
